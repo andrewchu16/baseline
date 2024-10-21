d4e542776479b1b268914538b38794f9ac75e333
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-10-21 14:24:29 -0400

    EVERYTHING WORKING but no styling

--- frontend/src/components/DcGraph.jsx
+++ frontend/src/components/DcGraph.jsx
@@ -1,14 +1,84 @@
-function DcGraph({ data }) {
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const width = 500 - margin.left - margin.right;
-    const height = 300 - margin.top - margin.bottom;
-  
-  
-    return (
-      <div className="bg-sky-200 rounded-lg p-2">
-      dc graph
-      </div>
-    )
-  }
-  
-  export default DcGraph;
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid,
+  ReferenceLine,
+} from "recharts";
+
+function DcGraph() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [threshold, setThreshold] = useState(0);
+
+  const fetchThreshold = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/threshold");
+
+      setThreshold(response.data.threshold);
+    } catch (err) {
+      setError("Failed to fetch EEG status.");
+      console.error("Error fetching status:", err);
+    }
+  };
+
+  const fetchDcs = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/status");
+
+      if (!response.data.initialized) {
+        setError("Baseline not initialized.");
+        return;
+      }
+
+      if (!response.data.processed) {
+        setError("No brain activity recorded.");
+        return;
+      }
+    } catch (err) {
+      setError("Failed to fetch EEG status.");
+      console.error("Error fetching status:", err);
+    }
+
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/dcs");
+      const d = response.data.dcs;
+
+      const chartData = d.map((value, index) => ({
+        name: String(index),
+        dc: value,
+      }));
+      setData(chartData);
+      console.log(d);
+      setError(null);
+    } catch (err) {
+      setError("Failed to fetch EEG alpha rhythms.");
+      console.error("Error fetching status:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchDcs();
+    fetchThreshold();
+  }, []);
+
+  return (
+    <div className="bg-sky-200 rounded-lg p-2">
+      <BarChart width={730} height={250} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="dc" fill="#8884d8" />
+        <ReferenceLine y={threshold} stroke="red" />
+      </BarChart>
+    </div>
+  );
+}
+
+export default DcGraph;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Rethink Sans', 'system-ui']
+    },
     extend: {},
   },
   plugins: [],
