de9bce83cda235b017f9288c856adb02d4fd9ef7
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-10-21 17:48:00 -0400

    styled home/landing page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 function Home() {
-    return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center bg-sky-200">
-            <h1 className="text-2xl mb-2">Baseline</h1>
-            <a href="/dashboard">
-            <button className="py-3 px-4 hover:translate-y-2 transition-transform shadow-md rounded-lg bg-white">connect headset</button>
-            </a>
-        </div>
-    );
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-neutral-200">
+      <h1 className="text-5xl mb-2 font-bold">Baseline</h1>
+      <p className="mb-4 text-lg">
+        The solution to fatigue-induced medical errors in healthcare
+        professionals.
+      </p>
+      <a href="/dashboard">
+        <button className="py-3 px-4 hover:translate-y-2 transition-transform shadow-md rounded-lg text-lg bg-neutral-100 hover:text-blue-600 text-blue-800">
+          Connect Headset
+        </button>
+      </a>
+    </div>
+  );
 }
 
 export default Home;
